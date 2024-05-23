ad85c33f1e19ce0ca35347fcf99f6d928b0110dc
ismael <ismaelreyplata@gmail.com>
2024-05-23 15:03:22 +0200

    antes de instalar primeng

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { InformeComponent } from './informe/informe.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo:'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'informe', component: InformeComponent }
+
+];
---
+++ src/app/informe/informe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-informe',
+  standalone: true,
+  imports: [],
+  templateUrl: './informe.component.html',
+  styleUrl: './informe.component.scss'
+})
+export class InformeComponent {
+
+}
